635c0ffa1469bd157aa7cc8bfb9cf75392d89903
m4chado <enzo653fm@gmail.com>
2023-03-18 09:35:50 -0300

    Feat: created nav components. Pending

---
+++ app/components/Nav.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import Login from "../auth/Login";
+import Logged from "../auth/Logged";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "../../pages/api/auth/[...nextauth]";
+
+export default async function Nav() {
+	const session = await getServerSession(authOptions);
+	return (
+		<nav className="flex justify-between items-center py-8">
+			<Link href={"/"}>
+				<h1 className="font-bold text-lg">Send it.</h1>
+			</Link>
+			<div className="flex items-center gap-6">
+				{!session?.user && <Login />}
+				{session?.user && <Logged image={session.user?.image || ""} />}
+			</div>
+		</nav>
+	);
+}
